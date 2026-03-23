a696a03f58be17ea446f879dcf75855be66ce8f7
Munkhuujin <itsmunkhush@gmail.com>
2026-03-23 21:12:08 +0800

    Lab07: Typescript

--- src/ordering/prioritization/recentmistakes.ts
+++ src/ordering/prioritization/recentmistakes.ts
@@ -2,25 +2,24 @@ import { CardStatus } from '../../cards/cardstatus.js'
 import { CardOrganizer } from '../cardorganizer.js'
 
 function newRecentMistakesFirstSorter (): CardOrganizer {
-  /**
-   * Computes the most recent mistake's time stamp for a card and helps in
-   * determining the sequence of cards in the next iteration, based on the
-   * rules that those answered incorrectly in the last round appear first.
-   *
-   * @param cardStatus The {@link CardStatus} object with failing
-   * @return The most recent incorrect response time stamp
-   */
   return {
-    /**
-     * Orders the cards by the time of most recent incorrect answers provided for them.
-     *
-     * @param cards The {@link CardStatus} objects to order.
-     * @return The ordered cards.
-     */
     reorganize: function (cards: CardStatus[]): CardStatus[] {
-      return []
+      const c = cards.slice()
+      c.sort((a, b) => {
+        const resultsA = a.getResults()
+        const resultsB = b.getResults()
+
+        const lastA = resultsA[resultsA.length - 1]
+        const lastB = resultsB[resultsB.length - 1]
+
+        // Boolean-г шууд шалгах (ts-standard-д нийцүүлсэн)
+        if (!lastA && lastB) return -1
+        if (lastA && !lastB) return 1
+        return 0
+      })
+      return c
     }
   }
-};
+}
 
 export { newRecentMistakesFirstSorter }
